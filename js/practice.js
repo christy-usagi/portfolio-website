9ea070163b8e66fae2adee7128d6c3c1ef02720f
christy-usagi <59301386+christy-usagi@users.noreply.github.com>
2020-11-18 00:13:42 -0800

    update

--- js/practice.js
+++ js/practice.js
@@ -2,10 +2,4 @@
   alert("Welcome!");
 </script>
 
-
-var text1 = "Hello";
-var text2 = "Hello, spaces are also OK!";
-var text3 = 'Single quotes are valid too.';
-
-var escapedText1 = "He said: \"Yes!\", that's for sure";
-var escapedText2 = 'He said: "Yes!", that\'s for sure';
+onclick="myFunction()"
