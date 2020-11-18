d6fd0ca705f93aef769c60cf2e2d8cd2e408bd2c
christy-usagi <59301386+christy-usagi@users.noreply.github.com>
2020-11-18 02:01:02 -0800

    update

--- js/practice.js
+++ js/practice.js
@@ -3,3 +3,13 @@
 </script>
 
 onclick="myFunction()"
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
