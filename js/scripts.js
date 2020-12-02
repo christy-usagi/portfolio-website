38afe87f634a504f87c732720082d071a76f21f9
christy-usagi <59301386+christy-usagi@users.noreply.github.com>
2020-12-01 16:09:14 -0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
-<script>
-  alert("Welcome!");
-</script>
-
-onclick="myFunction()"
-
-
-
-
-
+alert("Welcome!");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
     x.className = 'navtoggle';
   }
